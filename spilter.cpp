fc14dda7e151b05b3627314569d359fb2dd33a2a
Helloworld <47266067+YHTB@users.noreply.github.com>
2019-10-09 18:21:25 +0800

    Update spilter.cpp

--- spilter.cpp
+++ spilter.cpp
@@ -1,1 +1,26 @@
 
+
+std::vector<std::string*>  SpiltStr(const std::string& TotalStr, const char& spiltChar)
+{
+	std::vector<std::string *> SpiliterResult;
+	std::string tmpStr;
+	for (unsigned int i = 0; i <= TotalStr.length(); ++i)
+	{
+		if (TotalStr[i] != spiltChar)
+		{
+			tmpStr.push_back(TotalStr[i]);
+		}
+		else
+		{
+			SpiliterResult.push_back(new std::string(tmpStr));
+			tmpStr.clear();
+		}
+	}
+	if (tmpStr.length())
+	{
+		SpiliterResult.push_back(new std::string(tmpStr));
+	}
+
+
+	return SpiliterResult;
+}
